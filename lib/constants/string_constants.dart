b3890b4a733d47def7b36a0ba4672ab54034960d
Akhil <akhilvkm1991@gmail.com>
2022-01-09 19:12:51 +0530

    login and signup logic complete

--- lib/constants/string_constants.dart
+++ lib/constants/string_constants.dart
@@ -6,6 +6,7 @@ String splashImage = "assets/images/TechBlaze_logo.png";
 String appName = "Share Notes";
 
 var loginURL = Uri.parse("https://tb-share-note.herokuapp.com/api/login");
+var signupURL = Uri.parse("https://tb-share-note.herokuapp.com/api/register");
 
 IconData passwordVisibility = Icons.visibility;
 IconData passwordVisibilityOff = Icons.visibility_off;
--- lib/screens/signup/utility/functions.dart
+++ lib/screens/signup/utility/functions.dart
@@ -1,16 +1,50 @@
 // Sign Up onclick Function
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tb_share_notes/constants/string_constants.dart';
 import 'package:tb_share_notes/constants/style_constants.dart';
 import 'package:tb_share_notes/screens/home/home_screen.dart';
 import 'package:tb_share_notes/utility/validator.dart';
 
 void checkSignUp(BuildContext ctx, String _userName, String _eMail,
-    String _password, String _confirmPassord) {
+    String _password, String _confirmPassord) async {
+  Map data = {'name':_userName,'email': _eMail, 'password': _password,'confirm_password':_confirmPassord};
+  String _msg;
+  final sharedPreferences = await SharedPreferences.getInstance();
   if (_password == _confirmPassord) {
-    Navigator.pushReplacement(
-      ctx,
-      MaterialPageRoute(builder: (context) => const HomeScreen()),
-    );
+    var jsonData;
+    var response = await http.post(signupURL, body: data);
+    //_msg=jsonData['message'].toString();
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      jsonData = json.decode(response.body.toString());
+      await sharedPreferences.setString('token', jsonData['data']['token']);
+       ScaffoldMessenger.of(ctx).showSnackBar(
+         SnackBar(
+          content: Text("Success"),
+          behavior: SnackBarBehavior.fixed,
+        ),
+      );
+      Navigator.of(ctx).pushAndRemoveUntil(
+          MaterialPageRoute(
+              builder: (BuildContext context) => const HomeScreen()),
+          (Route<dynamic> route) => false);
+    } else {
+      ScaffoldMessenger.of(ctx).showSnackBar(
+        const SnackBar(
+          content: Text("Registration Failed"),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+    }
+
+    // Navigator.pushReplacement(
+    //   ctx,
+    //   MaterialPageRoute(builder: (context) => const HomeScreen()),
+    // );
   } else {
     ScaffoldMessenger.of(ctx).showSnackBar(
       const SnackBar(
